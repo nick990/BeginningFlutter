4633ebbe565f18dee13dcb44adf0f6dc473ee8db
nicolo <nicolo.festa90@gmail.com>
2020-08-23 22:25:17 +0200

    chapter 6 AppBar

---
+++ ch6_basics/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:ch6_basics/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Starter Template',
+      theme: ThemeData(
+        primaryColor: Colors.lightGreen,
+      ),
+      home: Home(),
+    );
+  }
+}

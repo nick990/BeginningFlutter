729ec168317c8cb7fc93c8a95dbcaf0389144de1
nicolo <nicolo.festa90@gmail.com>
2020-08-27 00:03:51 +0200

    Added columns and rows.

--- ch6_basics/lib/pages/home.dart
+++ ch6_basics/lib/pages/home.dart
@@ -37,7 +37,173 @@ class _HomeState extends State<Home> {
           preferredSize: Size.fromHeight(75.0),
         ),
       ),
-      body: Container(),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SafeArea(
+          child: SingleChildScrollView(
+              child: Column(
+            children: <Widget>[
+              const ContainerWithBoxDecorationWidget(),
+              Divider(),
+              const ColumnWidget(),
+              Divider(),
+              const RowWidget(),
+              Divider(),
+              const ColumnAndRowNestingWidget(),
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
+
+class ContainerWithBoxDecorationWidget extends StatelessWidget {
+  const ContainerWithBoxDecorationWidget({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Container(
+        height: 100.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(100.0),
+            bottomRight: Radius.circular(10.0),
+          ),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.white,
+              Colors.lightGreen.shade500,
+            ],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 10.0,
+              offset: Offset(0.0, 10.0),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Center(
+            child: RichText(
+              text: TextSpan(
+                text: 'Flutter World',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.deepPurple,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.deepPurpleAccent,
+                  decorationStyle: TextDecorationStyle.dotted,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.normal,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: ' for',
+                  ),
+                  TextSpan(
+                    text: ' Mobile',
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      )
+    ]);
+  }
+}
+
+class ColumnWidget extends StatelessWidget {
+  const ColumnWidget({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Text('Column 1'),
+        Divider(),
+        Text('Column 2'),
+        Divider(),
+        Text('Column 3'),
+      ],
+    );
+  }
+}
+
+class RowWidget extends StatelessWidget {
+  const RowWidget({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Row(
+          children: [
+            Text('Row1'),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+            ),
+            Text('Row2'),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+            ),
+            Text('Row3'),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ColumnAndRowNestingWidget extends StatelessWidget {
+  const ColumnAndRowNestingWidget({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Text(
+          'Columns and Row Nesting 1',
+        ),
+        Text(
+          'Columns and Row Nesting 2',
+        ),
+        Text(
+          'Columns and Row Nesting 3',
+        ),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Row Nesting 1'),
+            Text('Row Nesting 2'),
+            Text('Row Nesting 3'),
+          ],
+        ),
+      ],
     );
   }
 }

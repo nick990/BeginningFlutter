fd5964fd9caf9c45708852a14fedfe1a241f07ef
nicolo <nicolo.festa90@gmail.com>
2020-08-29 15:59:59 +0200

    Form

--- ch6_form_validation/lib/pages/home.dart
+++ ch6_form_validation/lib/pages/home.dart
@@ -6,13 +6,77 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
+  Order _order = Order();
+
+  String _validateItemRequired(String value) {
+    return value.isEmpty ? 'Item Required' : null;
+  }
+
+  String _validateItemCount(String value) {
+    int _valueAsInteger = value.isEmpty ? 0 : int.tryParse(value);
+    return _valueAsInteger == 0 ? 'At least one Item is Required' : null;
+  }
+
+  void _submitOrder() {
+    if (_formStateKey.currentState.validate()) {
+      _formStateKey.currentState.save();
+      print('Order Item: ${_order.item}');
+      print('Order Quantity: ${_order.quantity}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Form Validation'),
       ),
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Form(
+              key: _formStateKey,
+              autovalidate: true,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Espresso', labelText: 'Item'),
+                      validator: (value) => _validateItemRequired(value),
+                      onSaved: (value) => _order.item = value,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: '3',
+                        labelText: 'Quantity',
+                      ),
+                      keyboardType: TextInputType.number,
+                      validator: (value) => _validateItemCount(value),
+                      onSaved: (value) => _order.quantity = int.tryParse(value),
+                    ),
+                    Divider(
+                      height: 32.0,
+                    ),
+                    RaisedButton(
+                      child: Text('Save'),
+                      color: Colors.lightGreen,
+                      onPressed: () => _submitOrder(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+class Order {
+  String item;
+  int quantity;
 }
